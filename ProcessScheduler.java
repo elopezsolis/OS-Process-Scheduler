1d9e98ce57b12eccc498b6d7d734cbc04e41a9ca
Erick <elopezsolis@cpp.edu>
2017-05-04 10:46:32 -0700

    Added PrintTAT which prints the turn around time

--- ProcessScheduler.java
+++ ProcessScheduler.java
@@ -16,6 +16,9 @@ public class ProcessScheduler {
         ps.FCFS();
     }
     
+    public void printTAT(double turnArTi, int size){
+        System.out.printf("Average turnaround time: %.2f",turnArTi/(double)size);
+    }
 
     public void FCFS(){
         int counter= 0;
@@ -27,25 +30,20 @@ public class ProcessScheduler {
             turnaroundTime += counter;
             this.queue.remove(0);
         }
-        System.out.printf("Average turnaround time: %.2f",turnaroundTime/(double)size);
-
+        this.printTAT(turnaroundTime,size);
     }
+
     public ProcessScheduler(){
         this.queue = new ArrayList<>();
     }
+
     public void ReadFile(String fileName){
         try{
             Scanner fileIn = new Scanner(new File(fileName));
-
             while(fileIn.hasNext()){
                 String[] temp = fileIn.next().split(",");
                 this.queue.add(new Process(Integer.parseInt(temp[0]),Integer.parseInt(temp[1])));
             }
-
         }catch(FileNotFoundException ex ){ex.printStackTrace();}
-
-
-
-
     }
 }
