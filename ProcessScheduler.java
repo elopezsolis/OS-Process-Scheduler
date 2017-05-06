cff16c00809fb0042b7a1f0517ffcaf35abb25eb
Erick <elopezsolis@cpp.edu>
2017-05-05 19:33:08 -0700

    Created Random Scheduler. Picks a process randomly depending on its probability.

--- ProcessScheduler.java
+++ ProcessScheduler.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
@@ -14,15 +15,16 @@ public class ProcessScheduler {
         ArrayList<Process> queue;
         ProcessScheduler ps = new ProcessScheduler();
         queue = ps.ReadFile("in.txt");
-        System.out.println(queue.toString());
         ps.FCFS(queue);
         ps.SFS(queue);
         ps.RRS(queue,50);
         ps.RRS(queue,100);
+        ps.computeProbability(queue);
+        ps.RandomScheduler(queue,50);
     }
     
     public void printTAT(double turnArTi, int size){
-        System.out.printf("Average turnaround time: %.2f\n",turnArTi/(double)size);
+        System.out.printf("Average turnaround time: %.2f\n\n",turnArTi/(double)size);
     }
 
     /**
@@ -47,16 +49,54 @@ public class ProcessScheduler {
 
     public ProcessScheduler(){}
 
-    public ArrayList<Process> ReadFile(String fileName){
-        ArrayList<Process> queue = new ArrayList<>();
-        try{
-            Scanner fileIn = new Scanner(new File(fileName));
-            while(fileIn.hasNext()){
-                String[] temp = fileIn.next().split(",");
-                queue.add(new Process(Integer.parseInt(temp[0]),Integer.parseInt(temp[1])));
+    public void RandomScheduler(ArrayList<Process> queue, int quantum){
+        ArrayList<Process> list = new ArrayList<>(queue);
+        int counter =0;
+        int total =0;
+        System.out.println("Running Random Scheduler with quantum 50");
+        while(!list.isEmpty()){
+            int next = this.nextItem(list);
+            Process current = list.get(next);
+
+            if(current.getCycles() <= quantum) {
+                counter += current.getCycles();
+                System.out.println("Process " + current.getPID() + " finishes on cycle " + counter);
+                total += counter;
+                list.remove(next);
             }
-        }catch(FileNotFoundException ex ){ex.printStackTrace();}
-        return queue;
+            else {
+                current.setCycles(current.getCycles() - quantum);
+                counter += quantum;
+            }
+        }
+        this.printTAT(total,queue.size());
+
+    }
+    public int nextItem(ArrayList<Process> list){
+        double rand = Math.random();
+        double[] probArr = this.computeProbability(list);
+        double commProb =0.0;
+
+        for(int i =0;i<list.size();i++){
+            commProb += probArr[i];
+            if(rand <= commProb)
+                return i;
+        }
+        return -1;
+
+    }
+    public double[] computeProbability(ArrayList<Process> list){
+        double[] prob = new double[list.size()];
+        double total = 0;
+        for ( int i =0; i <list.size();i++){
+            total += list.get(i).getCycles();
+        }
+
+        for(int i=0;i<list.size();i++){
+            prob[i] = list.get(i).getCycles()/total;
+        }
+
+        return prob;
     }
 
     /**
@@ -101,4 +141,16 @@ public class ProcessScheduler {
         }
         this.printTAT(total,size);
     }
+    public ArrayList<Process> ReadFile(String fileName){
+        ArrayList<Process> queue = new ArrayList<>();
+        try{
+            Scanner fileIn = new Scanner(new File(fileName));
+            while(fileIn.hasNext()){
+                String[] temp = fileIn.next().split(",");
+                queue.add(new Process(Integer.parseInt(temp[0]),Integer.parseInt(temp[1])));
+            }
+        }catch(FileNotFoundException ex ){ex.printStackTrace();}
+        return queue;
+    }
+
 }
